fcb89cd58ae3e9029edb2e1dd07bdb522c8e75cd
George Antoniadis <george@noodles.gr>
2012-11-26 16:18:44 +0200

    Scala by @dimmu

---
+++ fizzbuzz.scala
@@ -0,0 +1,11 @@
+// FizzBuzz Solution for Scala
+//  - submitted by @dimmu
+
+(1 to 100) map { x => 
+	(x % 3, x % 5) match {
+		case (0,0) => "FizzBuzz"
+		case (0,_) => "Fizz" 
+		case (_,0) => "Buzz" 
+		case _ => x toString 
+	}
+} foreach println
